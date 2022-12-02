9b68f2c21159e8398f5b9f31dc3290688c193f60
seregov-sd <seregov.sd@yandex.by>
2022-12-03 02:47:47 +0300

    feat: implemented from getPositivesCount function in task 03-arrays-tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -305,8 +305,8 @@ function get3TopItems(/* arr */) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  return arr.filter((value) => typeof value === 'number' && value > 0).length;
 }
 
 /**
