346438de5d1ce952b6deff27df17f3aebcd7140c
seregov-sd <seregov.sd@yandex.by>
2022-12-02 03:00:31 +0300

    feat: implemented from getlastdigit function in task 02-numbers-tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -117,7 +117,7 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  * Returns a last digit of a integer number.
  *
  * @param {number} value
- * @return {number}
+ * @return {string}
  *
  * @example:
  *   100     => 0
@@ -125,8 +125,9 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const numbers = value.toString(10).split('');
+  return numbers[numbers.length - 1];
 }
 
 
